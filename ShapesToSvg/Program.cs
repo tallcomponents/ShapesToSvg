dae6233d7b970e824b5790f9ffa8b90368cae9d0
Frank Rem <frank@tallcomponents.com>
2015-09-24 16:07:16 +0200

    Initial import of existing project.

---
+++ ShapesToSvg/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+using TallComponents.PDF;
+using R = TallComponents.PDF.Rasterizer;
+
+namespace ExtractShapes
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         string[] fileNames = new string[]  
+         {
+            "oval", "3BigPreview90", "0703 Base-11", "5242_C3.0-0_Bid_Set"
+         };
+
+         foreach (string fileName in fileNames)
+         {
+            convert(fileName);
+         }
+      }
+
+      static void convert(string fileName)
+      {
+         using (FileStream inFile = new FileStream(string.Format(@"..\..\{0}.pdf", fileName), FileMode.Open, FileAccess.Read))
+         {
+            Document document = new Document(inFile);
+
+            foreach (var page in document.Pages)
+            {
+               var viewerTransform = GetViewerTransform(page);
+
+               int pagenr = page.Index + 1;
+
+               using (FileStream outFile = new FileStream(string.Format(@"..\..\{0}_{1}.html", fileName, pagenr), FileMode.Create, FileAccess.Write))
+               {
+                  using (StreamWriter outStream = new StreamWriter(outFile))
+                  {
+                     double viewerWidth = page.Orientation == Orientation.Rotate0 || page.Orientation == Orientation.Rotate180 ? page.Width : page.Height;
+                     double viewerHeight = page.Orientation == Orientation.Rotate0 || page.Orientation == Orientation.Rotate180 ? page.Height : page.Width;
+                     var writer = new ShapeWriter(outStream, viewerTransform);
+                     writer.Start(viewerWidth, viewerHeight);
+                     var shapes = page.CreateShapes();
+                     writer.WriteShape(shapes, new Matrix());
+                     writer.End();
+                  }
+               }
+            }
+         }
+      }
+
+      // It creates a tranformation which handles rotation and
+      // transforms PDF coordinate space into scree ncoordinate space
+      private static Matrix GetViewerTransform(Page page)
+      {
+         var mediaBox = page.MediaBox;
+
+         double height;
+         double dx, dy;
+         int rotate;
+
+         switch (page.Orientation)
+         {
+            case Orientation.Rotate0:
+            default:
+               height = mediaBox.Height;
+               rotate = 0;
+               dx = 0;
+               dy = 0;
+               break;
+            case Orientation.Rotate90:
+               height = mediaBox.Width;
+               rotate = 90;
+               dx = 0;
+               dy = -mediaBox.Height;
+               break;
+            case Orientation.Rotate180:
+               height = mediaBox.Height;
+               rotate = 180;
+               dx = -mediaBox.Width;
+               dy = -mediaBox.Height;
+               break;
+            case Orientation.Rotate270:
+               height = mediaBox.Width;
+               rotate = 270;
+               dx = -mediaBox.Width;
+               dy = 0;
+               break;
+         }
+
+         Matrix matrix = new Matrix();
+
+         // From PDF coordinate system (zero is at the bottom) to screen (zero is at the top)
+         matrix.Translate(0, (float)height);
+         matrix.Scale(1, -1);
+
+         // Rotation
+         matrix.Rotate(rotate);
+         matrix.Translate((float)dx, (float)dy);
+
+
+         // TODO: cropbox translation should be handled as well
+         return matrix;
+      }
+   }
+}
---
+++ ShapesToSvg/ShapeWriter.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.IO;
+
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+using TallComponents.PDF.Shapes;
+
+namespace ExtractShapes
+{
+   public class ShapeWriter
+   {
+      StreamWriter outStream;
+      Matrix viewerTransform;
+
+      public ShapeWriter(StreamWriter outStream, Matrix viewerTransform)
+      {
+         this.outStream = outStream;
+         this.viewerTransform = viewerTransform;
+      }
+
+      private string GenPoint(double x, double y, Matrix transform)
+      {
+         PointF[] points = { new PointF((float)x, (float)y) };
+
+         transform.TransformPoints(points);
+         viewerTransform.TransformPoints(points);
+
+         return String.Format("{0} {1}", points[0].X, points[0].Y);
+      }
+
+      private System.Drawing.Rectangle TransformBox(double x, double y, double w, double h, Matrix transform)
+      {
+         PointF[] points = { new PointF((float)x, (float)y), new PointF((float)(x + w), (float)(y + h)) };
+
+         transform.TransformPoints(points);
+
+         var rect = new System.Drawing.Rectangle(
+                 (int)Math.Min(points[0].X, points[1].X),
+                 (int)Math.Min(points[0].Y, points[1].Y),
+                 (int)Math.Abs(points[0].X - points[1].X),
+                 (int)Math.Abs(points[0].Y - points[1].Y));
+
+         return rect;
+      }
+
+      private string GenBox(double x, double y, double w, double h, Matrix transform)
+      {
+         var rect = TransformBox(x, y, w, h, transform);
+
+         return String.Format("{0} {1} W{2} H{3}", rect.X, rect.Y, rect.Width, rect.Height);
+      }
+
+      private void DrawBox(double x, double y, double w, double h, Matrix transform)
+      {
+         var rect = TransformBox(x, y, w, h, transform);
+         //pageGraphics.DrawRectangle(Pens.Black, rect);
+      }
+
+      public void WriteShapes(ShapeCollection shapes, Matrix transform)
+      {
+         foreach (var shape in shapes)
+         {
+            WriteShape(shape, transform);
+         }
+      }
+
+      public void WriteShapes(LayerShape shapes, Matrix transform)
+      {
+         foreach (var shape in shapes)
+         {
+            WriteShape(shape, transform);
+         }
+      }
+
+      private void WriteFreeHandPath(FreeHandPath path, Matrix transform)
+      {
+         foreach (var segment in path.Segments)
+         {
+            if (segment is FreeHandStartSegment)
+            {
+               var s = (FreeHandStartSegment)segment;
+               outStream.Write("M {0} ", GenPoint(s.X, s.Y, transform));
+            }
+            else if (segment is FreeHandLineSegment)
+            {
+               var s = (FreeHandLineSegment)segment;
+               outStream.Write("L {0} ", GenPoint(s.X1, s.Y1, transform));
+            }
+            else if (segment is FreeHandBezierSegment)
+            {
+               var s = (FreeHandBezierSegment)segment;
+               outStream.Write("C {0} {1} {2} ",
+                   GenPoint(s.X1, s.Y1, transform),
+                   GenPoint(s.X2, s.Y2, transform),
+                   GenPoint(s.X3, s.Y3, transform));
+            }
+         }
+
+         if (path.Closed)
+         {
+            outStream.Write("Z ");
+         }
+      }
+
+      private void WriteFreeHandPath(FreeHandPathCollection paths, Matrix transform)
+      {
+         foreach (var path in paths)
+         {
+            WriteFreeHandPath(path, transform);
+         }
+      }
+
+      private void write(FreeHandShape freeHandShape, Matrix transform)
+      {
+         outStream.Write("<path stroke=\"blue\" stroke-wdith=\"3\" fill=\"none\" d=\"");
+         WriteFreeHandPath(freeHandShape.Paths, transform);
+         outStream.Write("\"/>\n");
+      }
+
+      public void Start(double width, double height)
+      {
+         outStream.Write("<!DOCTYPE html>\n<html>\n<body>\n<svg width=\"{0}\" height=\"{1}\">\n", width, height);
+      }
+
+      public void End()
+      {
+         outStream.Write("</svg></body></html>");
+      }
+
+      public void WriteShape(Shape shape, Matrix transform)
+      {
+         // ContentShape is a bse class of most shapes. It has an associated transformation. 
+         // First it is merged with the root one.
+         // The exact shaped based behavior is handled after that.
+         if (shape is TallComponents.PDF.Shapes.ContentShape)
+         {
+            ContentShape contentshape = (ContentShape)shape;
+
+            // Matrix multiplication is not commutative, we need to accumulate
+            // transformations from down to up in the tree (from leafs to root), so swap the matrices.
+            // This method works because matrix multiplication is associative.
+            Matrix newTransform = contentshape.Transform.CreateGdiMatrix();
+            newTransform.Multiply(transform);
+
+            if (shape is TallComponents.PDF.Shapes.FreeHandShape)
+            {
+               write(shape as FreeHandShape, newTransform);
+            }
+            else if (shape is TallComponents.PDF.Shapes.ShapeCollection)
+            {
+               WriteShapes((ShapeCollection)shape, newTransform);
+            }
+            else if (shape is TallComponents.PDF.Shapes.LayerShape)
+            {
+               WriteShapes((LayerShape)shape, newTransform);
+            }
+            
+            return;
+            
+            if (shape is TallComponents.PDF.Shapes.BezierShape)
+            {
+               var s = (BezierShape)shape;
+               outStream.Write("BezierShape: ");
+               outStream.Write("{{{0} {1} {2} {3}}}",
+                   GenPoint(s.X0, s.Y0, newTransform), GenPoint(s.X1, s.Y1, newTransform),
+                   GenPoint(s.X2, s.Y2, newTransform), GenPoint(s.X3, s.Y3, newTransform));
+               outStream.WriteLine();
+            }
+            else if (shape is TallComponents.PDF.Shapes.ClipShape)
+            {
+               var s = (ClipShape)shape;
+               outStream.Write("ClipShape: ");
+               WriteFreeHandPath(s.Paths, newTransform);
+               outStream.WriteLine();
+            }
+            else if (shape is TallComponents.PDF.Shapes.EllipseShape)
+            {
+               var s = (EllipseShape)shape;
+               outStream.Write("EllipseShape: ");
+               outStream.Write("{{C{0} RX{1} RY{2}}}",
+                   GenPoint(s.CenterX, s.CenterY, newTransform), s.RadiusX, s.RadiusY);
+               outStream.WriteLine();
+            }
+            else if (shape is TallComponents.PDF.Shapes.ImageShape)
+            {
+               var s = (ImageShape)shape;
+               outStream.Write("ImageShape: ");
+               outStream.Write("{{S{0}}}", GenBox(s.X, s.Y, s.Width, s.Height, newTransform));
+               outStream.WriteLine();
+            }
+            else if (shape is TallComponents.PDF.Shapes.LineShape)
+            {
+               var s = (LineShape)shape;
+               outStream.Write("LineShape: ");
+               outStream.Write("{{S{0} L{1}}}",
+                   GenPoint(s.StartX, s.StartY, newTransform),
+                   GenPoint(s.EndX, s.EndY, newTransform));
+               outStream.WriteLine();
+            }
+            else if (shape is TallComponents.PDF.Shapes.RectangleShape)
+            {
+               var s = (RectangleShape)shape;
+               outStream.Write("RectangleShape: ");
+               outStream.Write("{{S{0}}}", GenBox(s.X, s.Y, s.Width, s.Height, newTransform));
+               outStream.WriteLine();
+            }
+            else if (shape is TallComponents.PDF.Shapes.TextShape)
+            {
+               var s = (TextShape)shape;
+               outStream.Write("TextShape: ");
+               outStream.Write("{{S{0} \"{1}\"}}",
+                   GenBox(s.BoundingBox.X, s.BoundingBox.Y, s.BoundingBox.Width, s.BoundingBox.Height, newTransform), s.Text);
+               outStream.WriteLine();
+
+               DrawBox(s.BoundingBox.X, s.BoundingBox.Y, s.BoundingBox.Width, s.BoundingBox.Height, newTransform);
+            }
+            else
+            {
+               outStream.Write("Unhandled shape: ");
+               outStream.WriteLine(shape.GetType().Name);
+            }
+         }
+      }
+   }
+}
